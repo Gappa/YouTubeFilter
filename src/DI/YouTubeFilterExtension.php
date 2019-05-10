70f42fac9f509e21496d81ebb59996f7222b1578
Pavel Linhart <pavel.linhart@gmail.com>
2019-05-10 11:22:06 +0200

    Update to Nette 3.0

--- src/DI/YouTubeFilterExtension.php
+++ src/DI/YouTubeFilterExtension.php
@@ -2,58 +2,33 @@
 
 namespace Nelson\Latte\Filters\YouTubeFilter\DI;
 
-use Latte\Engine;
 use Nelson\Latte\Filters\YouTubeFilter\YouTubeFilter;
-use Nette\Bridges\ApplicationLatte\ILatteFactory;
 use Nette\DI\CompilerExtension;
-use Nette\DI\ServiceDefinition;
+use Nette\DI\Definitions\FactoryDefinition;
 
 final class YouTubeFilterExtension extends CompilerExtension
 {
 
-	/** @var array */
-	protected $defaults = [];
-
-
-	public function loadConfiguration()
+	public function loadConfiguration(): void
 	{
 		$builder = $this->getContainerBuilder();
-		$config = $this->getConfig($this->defaults);
 		$builder->addDefinition($this->prefix('default'))
 			->setClass(YouTubeFilter::class);
 	}
 
 
-	public function beforeCompile()
+	public function beforeCompile(): void
 	{
 		$builder = $this->getContainerBuilder();
 
-		$registerToLatte = function (ServiceDefinition $def) {
-			$def->addSetup('addFilter', ['youtube', [$this->prefix('@default'), 'inline']]);
-		};
-
-		$latteFactoryService = $builder->getByType(ILatteFactory::class);
-		if (!$latteFactoryService || !self::isOfType($builder->getDefinition($latteFactoryService)->getClass(), Engine::class)) {
-			$latteFactoryService = 'nette.latteFactory';
+		// Latte filter
+		$latteFactoryName = 'latte.latteFactory';
+		if ($builder->hasDefinition($latteFactoryName)) {
+			/** @var FactoryDefinition $latteFactory */
+			$latteFactory = $builder->getDefinition($latteFactoryName);
+			$latteFactory
+				->getResultDefinition()
+				->addSetup('addFilter', ['youtube', [$this->prefix('@default'), 'inline']]);
 		}
-
-		if ($builder->hasDefinition($latteFactoryService) && self::isOfType($builder->getDefinition($latteFactoryService)->getClass(), Engine::class)) {
-			$registerToLatte($builder->getDefinition($latteFactoryService));
-		}
-
-		if ($builder->hasDefinition('nette.latte')) {
-			$registerToLatte($builder->getDefinition('nette.latte'));
-		}
-	}
-
-
-	/**
-	 * @param string $class
-	 * @param string $type
-	 * @return bool
-	 */
-	private static function isOfType($class, $type)
-	{
-		return $class === $type || is_subclass_of($class, $type);
 	}
 }
--- src/YouTubeFilter.php
+++ src/YouTubeFilter.php
@@ -5,25 +5,23 @@ namespace Nelson\Latte\Filters\YouTubeFilter;
 use Nette\SmartObject;
 use Nette\Utils\Html;
 
-/**
- * Usage: {='https://www.youtube.com/watch?v=WRt4wx-norQ', '800x450'|youtube}
- */
 class YouTubeFilter
 {
 	use SmartObject;
 
+	/** @var string */
 	const URL_REGEXP = '/^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/';
+
+	/** @var string */
 	const URL_EMBED = 'https://www.youtube.com/embed/';
-	const CSS_CLASS = 'yt-video';
+
+	/** @var string */
+	const CSS_CLASS = 'responsive-iframe';
 
 
-	/**
-	 * @param  string $url
-	 * @param  string $param
-	 * @return Html|void
-	 */
-	public static function inline($url, $param)
+	public static function inline(string $url, string $param): ?Html
 	{
+		/** @var array|false $dims */
 		$dims = explode('x', $param);
 
 		if (!empty($url)) {
@@ -31,15 +29,15 @@ class YouTubeFilter
 
 			if (isset($matches[7])) {
 				return Html::el('div')
-					->class(static::CSS_CLASS)
+					->setAttribute('class', static::CSS_CLASS)
 					->addHtml(
 						Html::el('iframe')
-							->src(static::URL_EMBED . $matches[7] . '?wmode=transparent')
-							->width($dims[0])
-							->height($dims[1])
-							->frameborder(0)
-							->wmode('opaque')
-							->allowfullscreen(true)
+						->setAttribute('src', static::URL_EMBED . $matches[7] . '?wmode=transparent')
+						->setAttribute('wmode', 'opaque')
+						->setAttribute('width', $dims[0])
+						->setAttribute('height', $dims[1])
+						->setAttribute('frameborder', 0)
+						->setAttribute('allowfullscreen', true)
 					);
 			}
 		}
